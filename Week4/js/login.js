35fd318e9af36f49c7e665f611443ec7f98d8ba1
BillyRayJoeBob <steven.morales@oit.edu>
2020-02-01 14:25:31 -0800

    Week4LabComplete
    Lab 4 Completed

--- Week4/js/login.js
+++ Week4/js/login.js
@@ -4,8 +4,8 @@ function validate() {
     document.getElementById('user-name-error').style.visibility = 'hidden'
     document.getElementById('Password-error').style.visibility = 'hidden'
 
-    let firstName = document.getElementById('UserName').value
-    let lastName = document.getElementById('Password').value
+    var firstName = document.getElementById('UserName').value
+    var lastName = document.getElementById('Password').value
 
     if (firstName == '') {
         document.getElementById('user-name-error').style.visibility = 'visible'
